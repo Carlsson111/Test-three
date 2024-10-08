7c9f51a57c85e3e16d0840d61f82dada451a1c6c
Carlsson111 <Linusjaa1@gmail.com>
2024-10-08 13:22:04 +0200

    Test with idead to push to github

---
+++ src/main/java/org/example/myapp.java
@@ -0,0 +1,36 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import java.util.Scanner;
+
+public class myapp {
+    public static void main(String[] args) {
+        int a,b,c,d,e,f,g,result;
+        a=45;
+        b=11;
+        c=12;
+        d=4;
+        e=24;
+        f=6;
+        g=55;
+
+        result=a+b;
+        System.out.println(result);
+
+        result = g-c;
+        System.out.println(result);
+
+        result = c*d;
+        System.out.println(result);
+
+        result = e/f;
+        System.out.println(result);
+
+
+    }
+
+}
+
